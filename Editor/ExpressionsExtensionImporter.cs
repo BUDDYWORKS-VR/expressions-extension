a7022e35876f2a6e21b8fa47546b5d318b4f6035
JustBuddy <8122264+JustBuddy@users.noreply.github.com>
2024-06-22 16:33:56 +0200

    0.2.6 - Add selection for synced and unsynced variants.

--- Editor/ExpressionsExtensionImporter.cs
+++ Editor/ExpressionsExtensionImporter.cs
@@ -6,22 +6,37 @@ namespace BUDDYWORKS.ExpressionsExtension
     public class PrefabSpawner : MonoBehaviour
     {
         // VRCF Prefab definitions
-        static string prefabEE_VRCF = "fb61f054361106040888ef1bb33b44ad";
+        static string prefabEE_UnSynced_VRCF = "fb61f054361106040888ef1bb33b44ad";
+        static string prefabEE_Synced_VRCF = "9c1b692672e052d44a475587f7d535fd";
         static string VRCF_Path = "Packages/com.vrcfury.vrcfury";
 
         // Toolbar Menu
-        [MenuItem("BUDDYWORKS/Expression Extension/Spawn Prefab... [VRCFury]", false, 0)]
-        [MenuItem("GameObject/BUDDYWORKS/Expression Extension/Spawn Prefab... [VRCFury]", false, 0)]
-        private static void SpawnEE()
+        [MenuItem("BUDDYWORKS/Expression Extension/Spawn Unsynced Prefab... [VRCFury]", false, 0)]
+        [MenuItem("GameObject/BUDDYWORKS/Expression Extension/Spawn Unsynced Prefab... [VRCFury]", false, 0)]
+        private static void SpawnEE_Unsynced()
         {
-            SpawnPrefab(prefabEE_VRCF);
+            SpawnPrefab(prefabEE_UnSynced_VRCF);
         }
 
+        [MenuItem("BUDDYWORKS/Expression Extension/Spawn Synced Prefab... [VRCFury]", false, 1)]
+        [MenuItem("GameObject/BUDDYWORKS/Expression Extension/Spawn Synced Prefab... [VRCFury]", false, 0)]
+        private static void SpawnEE_Synced()
+        {
+            SpawnPrefab(prefabEE_Synced_VRCF);
+        }
+
+        [MenuItem("BUDDYWORKS/Expression Extension/Sync cost: 25 memory", false , 20)]
+        private static void Placeholder()
+        {
+            Debug.Log("Using the Synced prefab costs 25 parameter memory in your avatar.");
+        }
         // Enable or disable menu items dynamically
 
-        [MenuItem("BUDDYWORKS/Expression Extension/Spawn Prefab... [VRCFury]", true)]
-        [MenuItem("GameObject/BUDDYWORKS/Expression Extension/Spawn Prefab... [VRCFury]", true)]
-        private static bool ValidateSpawnPE()
+        [MenuItem("BUDDYWORKS/Expression Extension/Spawn Unsynced Prefab... [VRCFury]", true)]
+        [MenuItem("GameObject/BUDDYWORKS/Expression Extension/Spawn Unsynced Prefab... [VRCFury]", true)]
+        [MenuItem("BUDDYWORKS/Expression Extension/Spawn Synced Prefab... [VRCFury]", true)]
+        [MenuItem("GameObject/BUDDYWORKS/Expression Extension/Spawn Synced Prefab... [VRCFury]", true)]
+        private static bool ValidateSpawnEE()
         {
             return AssetDatabase.IsValidFolder(VRCF_Path) != false;
         }

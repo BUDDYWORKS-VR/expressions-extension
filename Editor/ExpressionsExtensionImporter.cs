0988e3db818acb59d618bc30a28afd71d1bd1e0f
JustBuddy <8122264+JustBuddy@users.noreply.github.com>
2024-06-25 09:46:06 +0200

    0.3.0 - Added ModularAvatar Support - Currently only works on WD On Avatars.

--- Editor/ExpressionsExtensionImporter.cs
+++ Editor/ExpressionsExtensionImporter.cs
@@ -8,9 +8,12 @@ namespace BUDDYWORKS.ExpressionsExtension
         // VRCF Prefab definitions
         static string prefabEE_UnSynced_VRCF = "fb61f054361106040888ef1bb33b44ad";
         static string prefabEE_Synced_VRCF = "9c1b692672e052d44a475587f7d535fd";
+        static string prefabEE_UnSynced_MA = "f1456f8d0c8efe2439dbe13581b42857";
+        static string prefabEE_Synced_MA = "0742dd64b1be7054fb968129df31ad41";
         static string VRCF_Path = "Packages/com.vrcfury.vrcfury";
+        static string MA_Path = "Packages/nadena.dev.modular-avatar";
 
-        // Toolbar Menu
+        // Toolbar Menu - VRCF
         [MenuItem("BUDDYWORKS/Expression Extension/Spawn Unsynced Prefab... [VRCFury]", false, 0)]
         [MenuItem("GameObject/BUDDYWORKS/Expression Extension/Spawn Unsynced Prefab... [VRCFury]", false, 0)]
         private static void SpawnEE_Unsynced()
@@ -25,13 +28,28 @@ namespace BUDDYWORKS.ExpressionsExtension
             SpawnPrefab(prefabEE_Synced_VRCF);
         }
 
+        // Toolbar Menu - MA
+        [MenuItem("BUDDYWORKS/Expression Extension/Spawn Unsynced Prefab... [ModularAvatar]", false, 2)]
+        [MenuItem("GameObject/BUDDYWORKS/Expression Extension/Spawn Unsynced Prefab... [ModularAvatar]", false, 1)]
+        private static void SpawnEE_Unsynced_MA()
+        {
+            SpawnPrefab(prefabEE_UnSynced_MA);
+        }
+
+        [MenuItem("BUDDYWORKS/Expression Extension/Spawn Synced Prefab... [ModularAvatar]", false, 3)]
+        [MenuItem("GameObject/BUDDYWORKS/Expression Extension/Spawn Synced Prefab... [ModularAvatar]", false, 1)]
+        private static void SpawnEE_Synced_MA()
+        {
+            SpawnPrefab(prefabEE_Synced_MA);
+        }
+
         [MenuItem("BUDDYWORKS/Expression Extension/Sync cost: 25 memory", false , 20)]
         private static void Placeholder()
         {
             Debug.Log("Using the Synced prefab costs 25 parameter memory in your avatar.");
         }
-        // Enable or disable menu items dynamically
 
+        // Enable or disable menu items dynamically
         [MenuItem("BUDDYWORKS/Expression Extension/Spawn Unsynced Prefab... [VRCFury]", true)]
         [MenuItem("GameObject/BUDDYWORKS/Expression Extension/Spawn Unsynced Prefab... [VRCFury]", true)]
         [MenuItem("BUDDYWORKS/Expression Extension/Spawn Synced Prefab... [VRCFury]", true)]
@@ -39,6 +57,15 @@ namespace BUDDYWORKS.ExpressionsExtension
         private static bool ValidateSpawnEE()
         {
             return AssetDatabase.IsValidFolder(VRCF_Path) != false;
+        }
+
+        [MenuItem("BUDDYWORKS/Expression Extension/Spawn Unsynced Prefab... [ModularAvatar]", true)]
+        [MenuItem("GameObject/BUDDYWORKS/Expression Extension/Spawn Unsynced Prefab... [ModularAvatar]", true)]
+        [MenuItem("BUDDYWORKS/Expression Extension/Spawn Synced Prefab... [ModularAvatar]", true)]
+        [MenuItem("GameObject/BUDDYWORKS/Expression Extension/Spawn Synced Prefab... [ModularAvatar]", true)]
+        private static bool ValidateSpawnEE_MA()
+        {
+            return AssetDatabase.IsValidFolder(MA_Path) != false;
         }
 
         // Prefab Spawner
